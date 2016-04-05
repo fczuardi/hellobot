6bc6d70a0475b4deeaeb19be9e1a18758753287d
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-05 15:44:08 -0300

    example from ms site

--- watterfall.js
+++ watterfall.js
@@ -1,4 +1,7 @@
+var restify = require('restify');
 var builder = require('botbuilder');
+
+var server = restify.createServer();
 
 var bot = new builder.TextBot();
 
@@ -17,8 +20,26 @@ bot.add('/', [
     },
     function (session, results) {
         session.dialogData.answers.push(results.response);
-        session.send('Bar! ' + session.dialogData.answers);
+        builder.Prompts.confirm(session, 'Are you sure?');
+    },
+    function (session, results) {
+        console.log(results);
+        if (results.response === true) {
+            session.send('Bar! ' + session.dialogData.answers);
+        } else {
+            session.send('Ok, again then…');
+        }
     }
 ]);
 
-bot.listenStdin();
+// bot.listenStdin();
+
+server.use(bot.verifyBotFramework({
+    appId: process.env.APP_ID,
+    appSecret: process.env.APP_SECRET })
+);
+server.post('/v1/messages', bot.listen());
+
+server.listen(8080, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
